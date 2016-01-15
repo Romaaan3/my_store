9b64491ee5b5ee089295cc091544d9ca07e9ff25
romaaan3 <romaaan3@gmail.com>
2016-01-15 08:40:42 +0300

    First commit

---
+++ app/models/cart.rb
@@ -0,0 +1,8 @@
+class Cart < ActiveRecord::Base
+
+	has_many :positions
+	belongs_to :user
+
+ 	has_many :items, through: :positions
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+	
+
+	has_one  :cart
+	has_many :orders
+	has_many :comments
+
+end
